9d1ba9bcfeb0f43efd1a6e284c78f6c63d903dfe
Larry Samuels <larry_samuels@hotmail.com>
2019-02-23 18:55:23 -0500

    Add Store App
    View, templates and URLS updated

---
+++ bookstore/store/urls.py
@@ -0,0 +1,11 @@
+'''
+Created on Feb 23, 2019
+
+@author: lawrencesamuels
+'''
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.store, name='store'),
+]
