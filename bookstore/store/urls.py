8a0d5b5b8e42c7895b64a1e381f97c3d35085740
Larry Samuels <larry_samuels@hotmail.com>
2019-03-09 16:31:33 -0500

    Add author and reviews, models

--- bookstore/store/urls.py
+++ bookstore/store/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.store, name='store'),
+    url(r'^$', views.store, name='index'),
+    url(r'^book/(\d+)',views.book_details, name='book_details'),
 ]
