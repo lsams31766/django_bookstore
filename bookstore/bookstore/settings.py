fb4864730b0bd34734a557b68151909c062ea696
Larry Samuels <larry_samuels@hotmail.com>
2019-03-09 22:15:22 -0500

    Add file upload for book cover

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'social.apps.django_app.context_processors.backends',
                 'social.apps.django_app.context_processors.login_redirect',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,6 +124,11 @@ STATICFILES_FINDERS = (
   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
   'compressor.finders.CompressorFinder',
 )
+
+#uploaded files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = "/media/"
+
 
 #registration
 ACCOUNT_ACTIVATION_DAYS = 7 
